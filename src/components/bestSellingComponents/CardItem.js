8cf5fd9a6b8caca6123f279d0b9f33e7e90f5a48
ryfazrin <riansera515@gmail.com>
2021-08-07 13:08:46 +0800

    add drawer detail item CardItem

--- src/components/bestSellingComponents/CardItem.js
+++ src/components/bestSellingComponents/CardItem.js
@@ -1,24 +1,98 @@
-import {Card, CardActionArea, CardContent, CardMedia, Typography, makeStyles, CardActions, Button } from "@material-ui/core";
-import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+  makeStyles,
+  CardActions,
+  Button,
+  Drawer,
+  Divider,
+  CardHeader
+} from "@material-ui/core";
+import React from "react";
+import clsx from 'clsx';
 
 const useStyles = makeStyles(() => ({
-    root: {
-      maxWidth: 345,
-    },
-    media:{
-      height: 140,
-    },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: '0',
+    paddingTop: '56.25%', // 16:9
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
 }));
 
 function CardItem() {
-    const classes = useStyles();
-    return(
-      <Card className={classes.root}>
-        <CardActionArea component={Link} to="/best-selling">
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    bottom: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Card
+        lassName={classes.cardDrawer}>
+        <CardHeader
+          title="Pisang banana"
+          subheader="September 14, 2021"
+        />
+        <CardMedia
+          className={classes.media}
+          image="images/breakfast.jpg"
+          title="breakfast"
+        />
+        <Divider variant="middle" />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Rp. 100.000,-
+            </Typography>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Button variant="contained" color="secondary">
+              Tambah
+            </Button>
+          </CardActions>
+      </Card>
+    </div>
+  );
+
+  return (
+    <Card className={classes.root}>
+      <React.Fragment key="bottom">
+        <CardActionArea onClick={toggleDrawer("bottom", true)}>
           <CardMedia
             className={classes.media}
             image="images/breakfast.jpg"
-            title="breakfast" />
+            title="breakfast"
+          />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               Pisang banana
@@ -28,13 +102,21 @@ function CardItem() {
             </Typography>
           </CardContent>
         </CardActionArea>
-        <CardActions>
-          <Button size="small" color="secondary">
-            Tambah
-          </Button>
-        </CardActions>
-      </Card>
-    );
+      </React.Fragment>
+
+      <CardActions>
+        <Button variant="outlined" size="small" color="secondary" width='100%'>
+          Tambah
+        </Button>
+      </CardActions>
+
+      <Drawer
+        anchor="bottom"
+        open={state["bottom"]}
+        onClose={toggleDrawer("bottom", false)}
+      >{list('bottom')}</Drawer>
+    </Card>
+  );
 }
 
 export default CardItem;
