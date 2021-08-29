4804bdb4112be79f02215ee69d6c0e3ad6d88451
ryfazrin <riansera515@gmail.com>
2021-08-29 12:22:26 +0800

    add back route history

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { AppBar, Icon, IconButton, Toolbar, Typography, makeStyles } from "@material-ui/core";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,27 +9,53 @@ const useStyles = makeStyles((theme) => ({
         marginRight: theme.spacing(2),
     },
     title: {
-    flexGrow: 1,
+      flexGrow: 1,
     },
 }));
 
+
 function Header() {
     const classes = useStyles();
+
+    let location = useLocation();
+
+    const back = () => {
+      window.history.back();
+    }
+
+    const HeadCondition = () => {
+      // eslint-disable-next-line eqeqeq
+      if (location.pathname != location.pathname.match('/')){
+        return (
+          <>
+            <IconButton onClick={back} edge="start" className={classes.menuButton} color="inherit">
+                <Icon>arrow_back</Icon>
+            </IconButton>
+          </>
+        );
+      }
+
+        return null;
+      
+    }
+
     return(
         <div className={classes.root}>
-            <AppBar>
-                <Toolbar position="static">
-                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <Icon>menu</Icon>
-                    </IconButton> */}
-                    <Typography variant="h6" className={classes.title}>
-                        Ryris
-                    </Typography>
-                    <IconButton color="inherit" component={Link} to="/account">
-                        <Icon>account_circle</Icon>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
+          <AppBar>
+            <Toolbar position="static">
+
+              <HeadCondition />
+
+              <Typography variant="h6" className={classes.title}>
+                Ryris
+              </Typography>
+
+              <IconButton color="inherit" component={Link} to="/account">
+                <Icon>account_circle</Icon>
+              </IconButton>
+
+            </Toolbar>
+          </AppBar>
         </div>
     );
 }
